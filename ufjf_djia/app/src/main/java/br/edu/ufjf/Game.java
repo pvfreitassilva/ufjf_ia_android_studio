ea5b44dac372434bed83d26bc866aa0df16f6ea5
Paulo Vitor Freitas da Silva <pvfreitas@ice.ufjf.br>
2016-01-15 16:40:43 -0200

    Bugs

--- ufjf_djia/app/src/main/java/br/edu/ufjf/Game.java
+++ ufjf_djia/app/src/main/java/br/edu/ufjf/Game.java
@@ -4,14 +4,18 @@ import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.Window;
+import android.view.WindowManager;
 
 public class Game extends Activity{
 	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		//todo alterar orientação da tela
-		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+		//setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
 		
 		GameView gameView = new GameView(this);
 		
